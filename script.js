6931e875f64360a96197e46a3adf163d744af3c0
WeduTM <doritorresmolina@gmail.com>
2025-10-23 13:28:57 +0200

    cambio a crypto

--- script.js
+++ script.js
@@ -44,17 +44,12 @@ const chalkLogUl = document.getElementById('chalkLog');
 
 
 // --- Funciones para manejar IDs de usuario únicos ---
-function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
+
 
 function getUserId() {
     let userId = localStorage.getItem('tizasAlemUserId');
     if (!userId) {
-        userId = generateUUID();
+        userId = crypto.randomUUID();
         localStorage.setItem('tizasAlemUserId', userId);
     }
     return userId;
